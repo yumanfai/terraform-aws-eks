07022303ea0800cf87841ff5e5662d83f7a38e52
Wayne Yu <yumanfai@gmail.com>
2021-12-08 14:14:44 +0800

    move providers

--- versions.tf
+++ versions.tf
@@ -1,16 +1,5 @@
 ## requirements
 
 terraform {
-  required_providers {
-    kustomization = {
-      source  = "kbst/kustomization"
-      version = "0.7.1"
-    }
-  }
   required_version = ">= 0.13"
-}
-
-provider "kustomization" {
-  kubeconfig_path = var.kubeconfig_path
-  kubeconfig_raw = yamlencode(var.kubeconfig)
 }
